09554c591921bc39b7ff81f9cb453ec49e86df35
matisgarcia45-bit <matisgarcia45@gmail.com>
2025-12-14 14:20:16 -0500

    proyecto final

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def portafolio(request):
+    return render(request, "core/portafolio.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
